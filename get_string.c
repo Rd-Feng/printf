0c37293a5a766af1c4081791307ba20c19e59d96
dspham <dsvpham@gmail.com>
2018-07-28 16:35:39 -0700

    get string

---
+++ get_string.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+char *_putstr(char *s)
+{
+    char *ptr;
+	int i, j;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+        ;
+    }
+    ptr = (char *)malloc(i * sizeof(char) + 1);
+    if (ptr == NULL)
+    {
+        return (NULL);
+    }
+    for (j = 0; j < i; j++)
+    {
+        ptr[j] = s[j];
+    }
+    ptr[j] = '\0';
+    return (ptr);
+}
