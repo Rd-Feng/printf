c4766c3869ca2cb95887c344904c6206791c26f6
Rui Feng <394@holbertonschool.com>
2018-07-30 22:20:02 +0000

    betty doc

--- _printf.c
+++ _printf.c
@@ -99,7 +99,6 @@ int _printf(const char *format, ...)
  * die_with_error - free memory and exit process with exit code e
  * @buffer: output buffer to be free
  * @arg: argument buffer to be free
- * @e: error code to exit process
  */
 void die_with_error(char *buffer, char *arg)
 {
