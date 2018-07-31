a080934fc8ab54d58837da7f2fdb5e93470268ad
dspham <dsvpham@gmail.com>
2018-07-31 14:03:53 -0700

    added binary

--- get_sp.c
+++ get_sp.c
@@ -1,5 +1,68 @@
 #include <stdlib.h>
 /**
+ * move_left -removes leading 0's
+ * @a: buffer
+ * @size: size of array
+ * @num: number of digits in the binary number
+ */
+char *move_left(char *a, int size, int num)
+{
+	int i;
+	char *ptr;
+
+	for (i = 0, ptr = (a + size - num); i < num; i++, ptr++)
+		*(a + i) = *ptr;
+	*(a + num) = '\0';
+	return (a);
+}
+
+/**
+ * get_binary - converts an unsigned int to binary
+ * @n: integer
+ * Return: pointer to binary
+ */
+
+char *get_binary(unsigned int n)
+{
+	char *ptr = NULL;
+	unsigned int i, num = 0, size = 0;
+
+	if (n == 0)
+	{
+			ptr = malloc(2);
+			if (ptr)
+			{
+				ptr[0] = '0';
+				ptr[1] = '\0';
+			}
+			return (ptr);
+	}
+	size = sizeof(int) * 8 + 1;
+	ptr = malloc(size);
+	if (ptr)
+	{
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = '0';
+		}
+		ptr[size - 1] = '\0';
+
+		i = size - 2;
+		while (n != 0)
+		{
+			ptr[i] = n % 2 == 0 ? '0' : '1';
+			num++;
+			n = n / 2;
+			i--;
+		}
+		num++;
+		ptr = move_left(ptr, size, num);
+	}
+	return (ptr);
+}
+
+
+/**
  * get_char - returns a copied char
  * @c: char to be copied
  * Return: ptr of copied char
